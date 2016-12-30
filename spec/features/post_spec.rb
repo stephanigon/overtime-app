f2917a8f7cd0792fcc5c2995f78fe061f91c5bd4
Stephani Gonzalez <spgonza@live.com>
2016-12-30 15:11:18 -0700

    refactored post_spec tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  let(:user) { FactoryGirl.create(:user)}
+
+  let(:post) do
+    Post.create(date: Date.today, rationale: "Rationale", user_id: user.id)
+  end
+
   before do
-    @user = FactoryGirl.create(:user)
-    login_as(@user, :scope => :user)
+    login_as(user, :scope => :user)
   end
 
   describe 'index' do
@@ -27,9 +32,6 @@ describe 'navigate' do
     end
 
     it 'has a scope so that only post creators can see their posts' do
-      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
-
       other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
       post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
 
@@ -50,25 +52,30 @@ describe 'navigate' do
 
   describe 'delete' do
     it 'can be deleted' do
-      @post = FactoryGirl.create(:post)
-      @post.update(user_id: @user.id)
+      logout(:user)
+
+      delete_user = FactoryGirl.create(:user)
+      login_as(delete_user, :scope => :user)
+
+      post_to_delete = Post.create(date: Date.today, rationale: 'asdf', user_id: delete_user.id)
+
       visit posts_path
 
-      click_link("delete_post_#{@post.id}_from_index")
+      click_link("delete_post_#{post_to_delete.id}_from_index")
       expect(page.status_code).to eq(200)
     end
   end
 
   describe 'creation' do
-      before do
-          visit new_post_path
-      end
+    before do
+      visit new_post_path
+    end
 
-      it 'has a new form that can be reached' do
-          expect(page.status_code).to eq(200)
-      end
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
 
-      it 'can be created from new form page' do
+    it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
@@ -86,30 +93,24 @@ describe 'navigate' do
   end
 
   describe 'edit' do
-    before do
-    @edit_user = User.create(first_name: "asdf", last_name: 'asdf', email: 'asdfasdf@asdf.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
-     login_as(@edit_user, :scope => :user)
-     @edit_post = Post.create(date: Date.today, rationale: 'asdf', user_id: @edit_user.id)
-   end
+    it 'can be edited' do
+      visit edit_post_path(post)
 
-   it 'can be edited' do
-    visit edit_post_path(@edit_post)
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Edited Content"
+      click_on "Save"
 
-    fill_in 'post[date]', with: Date.today
-    fill_in 'post[rationale]', with: "Edited Content"
-    click_on "Save"
+      expect(page).to have_content("Edited Content")
+    end
 
-    expect(page).to have_content("Edited Content")
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authourized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authourized_user, :scope => :user)
+
+      visit edit_post_path(post)
+
+      expect(current_path).to eq(root_path)
+    end
   end
-
-  it 'cannot be edited by a non authorized user' do
-    logout(:user)
-    non_authourized_user = FactoryGirl.create(:non_authorized_user)
-    login_as(non_authourized_user, :scope => :user)
-
-    visit edit_post_path(@edit_post)
-
-    expect(current_path).to eq(root_path)
-  end
-end
 end
