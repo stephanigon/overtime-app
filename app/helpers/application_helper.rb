6795d6517d0c4f4f70b211edb44df37ac6015c0c
Stephani Gonzalez <spgonza@live.com>
2017-01-04 16:25:35 -0700

    updated homepage flow for admins and users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def admin_types
+    ['AdminUser']
+  end
+
   def active?(path)
     "active" if current_page?(path)
   end
@@ -9,18 +13,18 @@ module ApplicationHelper
 
   private
 
-    def status_span_generator status
-      case status
-      when 'submitted'
-        content_tag(:span, status.titleize, class: 'label label-primary')
-      when 'approved'
-        content_tag(:span, status.titleize, class: 'label label-success')
-      when 'rejected'
-        content_tag(:span, status.titleize, class: 'label label-danger')
-      when 'pending'
-        content_tag(:span, status.titleize, class: 'label label-primary')
-      when 'confirmed'
-        content_tag(:span, status.titleize, class: 'label label-success')
-      end
+  def status_span_generator status
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'label label-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'label label-danger')
+    when 'pending'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'confirmed'
+      content_tag(:span, status.titleize, class: 'label label-success')
     end
+  end
 end
