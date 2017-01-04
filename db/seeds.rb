478b1d85bdb959a5d746c77c67629694c1c5b159
Stephani Gonzalez <spgonza@live.com>
2017-01-04 11:52:00 -0700

    integrated validations and defaults for audit log attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,29 @@
 @user = User.create(email: "test@test.com",
-										password: "asdfasdf", 
-										password_confirmation: "asdfasdf", 
-										first_name: "Jon", 
-										last_name: "Snow", 
-										phone: "+13852197732")
+										password: "asdfasdf",
+										password_confirmation: "asdfasdf",
+										first_name: "Jon",
+										last_name: "Snow",
+										phone: "3852197732")
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", 
-								 password: "asdfasdf", 
-								 password_confirmation: "asdfasdf", 
-								 first_name: "Admin", 
-								 last_name: "Name",
-								 phone: "+13852197732")
+AdminUser.create(email: "admin@test.com",
+									password: "asdfasdf",
+									password_confirmation: "asdfasdf",
+									first_name: "Admin",
+									last_name: "Name",
+									phone: "3852197732")
 
 puts "1 Admin User created"
 
-
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+	AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
