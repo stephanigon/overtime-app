0e2639a82f016dd16ed1db853ffcf53b86f1d24f
Stephani Gonzalez <spgonza@live.com>
2017-01-05 14:20:42 -0700

    integrated basic manager notification email

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,8 +11,8 @@ class Post < ActiveRecord::Base
 
 	private
 
-		def update_audit_log
-			audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
-			audit_log.confirmed!
-		end
+	def update_audit_log
+		audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+		audit_log.confirmed!
+	end
 end
